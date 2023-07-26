480b8b3cf5d1edd758adc7c40fba5ae5cc841d3a
zhenglipeng <zhenglipeng@sensetime.com>
2023-07-26 11:30:26 +0800

    模板改造

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
-export default defineConfig(({ command, mode }) => {
-    return {
-        resolve: {
-            alias: {
-                'babylonjs': mode === 'development' ? 'babylonjs/babylon.max' : 'babylonjs'
-            }
-        }
-    };
-});
+let base = '/';
+if (process.env.NODE_ENV === 'production') {
+  // base = '/baby/';
+  base = 'https://cdn.jsdelivr.net/gh/lanyuechen/baby@gh-pages/';
+}
+
+export default defineConfig({
+  base,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'babylonjs': process.env.NODE_ENV === 'development' ? 'babylonjs/babylon.max' : 'babylonjs',
+    }
+  }
+})
