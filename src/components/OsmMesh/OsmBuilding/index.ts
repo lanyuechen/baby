37d229c8b54c70ad1f9105d2c93881ab230cb5dc
zhenglipeng <zhenglipeng@sensetime.com>
2023-08-16 18:21:34 +0800

    贴图调试

--- src/components/OsmMesh/OsmBuilding/index.ts
+++ src/components/OsmMesh/OsmBuilding/index.ts
@@ -3,12 +3,6 @@ import earcut from 'earcut';
 import Boundary from '@/components/Boundary';
 import type { BuildingData } from '@/components/OsmService/typing';
 
-// roof:orientation=along/across	对于带有屋脊的屋顶，假定屋脊与建筑物的最长边平行(roof:orientation=along)。但是可以使用此标签明确标注。
-// roof:height=*	建筑物的高度（即立面的高度）计算为建筑物的总height=*减去roof:height=*。
-// roof:angle=*	除了roof:height=*之外，还可以通过提供坡面的倾斜度（以度为单位）来间接指定屋顶高度。
-// roof:levels=*	尚未计入building:levels=*中的屋顶内楼层数。
-// roof:direction=*
-
 export default class OsmTile extends BABYLON.AbstractMesh {
   scene: BABYLON.Scene;
   boundary?: Boundary;
@@ -45,13 +39,20 @@ export default class OsmTile extends BABYLON.AbstractMesh {
   createMaterial(data: BuildingData) {
     const material = new BABYLON.PBRMaterial('buildingMaterial', this.scene);
 
-    material.albedoColor = new BABYLON.Color3(1, 1, 1);
     material.metallic = 0;
     material.roughness = 1.0;
     material.albedoColor = BABYLON.Color3.FromHexString(data.color);   // 设置建筑颜色
+    material.albedoTexture = new BABYLON.Texture('textures/buildings/facades/block_wall_diffuse.png', this.scene);
+    // material.metallicTexture
+    material.bumpTexture = new BABYLON.Texture('textures/buildings/facades/block_wall_normal.png', this.scene);
+    material.forceIrradianceInFragment = true;
 
     // this.boundary?.setBoundary(material);
 
     return material;
   }
 }
+
+// "facadeBlockWallDiffuse": {"url": "/textures/buildings/facades/block_wall_diffuse.png", "type": "image"},
+// "facadeBlockWallNormal": {"url": "/textures/buildings/facades/block_wall_normal.png", "type": "image"},
+// "facadeBlockWallMask": {"url": "/textures/buildings/facades/block_wall_mask.png", "type": "image"},
